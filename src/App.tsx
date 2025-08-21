03092954545bece8f655ca21a3d8bd1b56d218b2
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-08-22 00:12:29 +0900

    ✨ Feat: TDS 설정
    feat: TDS 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TDSMobileBedrockProvider } from "@toss-design-system/mobile-bedrock";
 import Header from "@/components/Header";
 import Home from "@/pages/Home";
 import GroupPage from "@/pages/GroupPage";
@@ -9,19 +10,21 @@ import LoginPage from "@/pages/LoginPage";
 
 function App() {
   return (
-    <Router>
-      <div className="max-w-[420px] mx-auto">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/loading" element={<LoadingPage />} />
-          <Route path="/group" element={<GroupPage />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <TDSMobileBedrockProvider>
+      <Router>
+        <div className="max-w-[420px] mx-auto">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/loading" element={<LoadingPage />} />
+            <Route path="/group" element={<GroupPage />} />
+            <Route path="/list" element={<ListPage />} />
+            <Route path="/landing" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </TDSMobileBedrockProvider>
   );
 }
 
